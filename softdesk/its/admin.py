f84e7ab71f204b830c44fdc3a06704c2b889e351
vianneyba <vianneyba@free.fr>
2022-04-16 19:48:18 +0200

    create models

---
+++ softdesk/its/admin.py
@@ -0,0 +1,28 @@
+from django.contrib import admin
+from .models import Issue, Project, Comment
+
+
+class IssueAdmin(admin.ModelAdmin):
+    list_display = (
+        'title', 'description', 'author',
+        'tag', 'status', 'project',
+        'created_at' 
+    )
+
+
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = (
+        'title', 'description', 'type_project',
+        'author'
+    )
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = (
+        'description', 'issues', 'author'
+    )
+
+
+admin.site.register(Issue, IssueAdmin)
+admin.site.register(Project, ProjectAdmin)
+admin.site.register(Comment, CommentAdmin)
---
+++ softdesk/its/models.py
@@ -0,0 +1,104 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+TAG = (
+    ('bug', 'bug'),
+    ('improvement', 'improvement'),
+    ('task', 'task')
+)
+
+STATUS = (
+    ('to_do', 'to do'),
+    ('in_progress', 'in progress'),
+    ('finished', 'finished')
+)
+
+TYPE = (
+    ('backend', 'backend'),
+    ('frontend', 'frontend'),
+    ('ios', 'ios'),
+    ('android', 'android')
+)
+
+
+class Issue(models.Model):
+    title = models.CharField(
+        max_length=100,
+        blank=False)
+
+    description = models.TextField(
+        max_length=1500,
+        blank=False)
+
+    author = models.ForeignKey(
+        User,
+        related_name='user',
+        on_delete=models.CASCADE)
+
+    tag = models.CharField(
+        choices=TAG,
+        max_length=20,
+        blank=False)
+
+    status = models.CharField(
+        choices=STATUS,
+        max_length=20,
+        blank=False)
+
+    project = models.ForeignKey(
+        'Project',
+        related_name='issues',
+        on_delete=models.CASCADE)
+
+    created_at = models.DateTimeField(
+        auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.title}'
+
+
+class Project(models.Model):
+    title = models.CharField(
+        max_length=100,
+        blank=False)
+
+    description = models.TextField(
+        max_length=1500,
+        blank=False)
+
+    type_project = models.CharField(
+        choices=TYPE,
+        max_length=20,
+        blank=False)
+
+    author = models.ForeignKey(
+        User,
+        related_name='project_author',
+        on_delete=models.CASCADE)
+
+    contributors = models.ManyToManyField(
+        User,
+        related_name='project_contributors')
+
+    def __str__(self):
+        return f'{self.title}'
+
+
+class Comment(models.Model):
+    description = models.TextField(
+        max_length=1500,
+        blank=False)
+
+    issues = models.ForeignKey(
+        'Issue',
+        related_name='comments',
+        on_delete=models.CASCADE)
+        
+    author = models.ForeignKey(
+        User,
+        related_name='comment_author',
+        on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.issues.title}'
