a48d95af5c8f7162d2fcc81820346b05c0b38334
vianneyba <vianneyba@free.fr>
2022-04-18 19:57:32 +0200

    add update project

--- softdesk/its/views.py
+++ softdesk/its/views.py
@@ -27,6 +27,26 @@ class ProjectViewset(ModelViewSet):
             return Response(
                 serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+    def update(self, request, pk, *args, **kwargs):
+        try:
+            project_instance = Project.objects.get(pk=pk)
+        except Project.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        tempdict = request.data.copy()
+        tempdict['author'] = self.request.user.id
+
+        serializer = ProjectSerializer(
+            project_instance,
+            data=tempdict)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response(
+                serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 class IssueViewset(ModelViewSet):
 
