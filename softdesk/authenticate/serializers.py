ddb03f327c20ea5699ee2ec70a77386d5b636c58
vianneyba <vianneyba@free.fr>
2022-04-23 15:13:28 +0200

    fix signup

--- softdesk/authenticate/serializers.py
+++ softdesk/authenticate/serializers.py
@@ -7,13 +7,13 @@ class UserSerializer(ModelSerializer):
     class Meta:
         model = User
         fields = [
-            'id', 'username', 'first_name',
+            'id', 'first_name',
             'last_name', 'email', 'password'
         ]
 
     def create(self, data):
         user = User.objects.create_user(
-            data['username'], data['email'], data['password'])
+            data['email'], data['email'], data['password'])
         user.first_name = data['first_name']
         user.last_name = data['last_name']
         user.save()
--- softdesk/authenticate/tests.py
+++ softdesk/authenticate/tests.py
@@ -1,3 +1,25 @@
-from django.test import TestCase
+from django.urls import reverse_lazy
+from rest_framework.test import APITestCase
+from django.contrib.auth.models import User
 
-# Create your tests here.
+
+class TestSignup(APITestCase):
+    url = reverse_lazy('signup')
+
+    def test_create(self):
+        self.assertFalse(User.objects.exists())
+        data = {
+            'first_name': 'maxime',
+            'last_name': 'forestier',
+            'email': 'maxime.forestier@free.fr',
+            'password': 'lille59000'
+        }
+
+        response = self.client.post(self.url, data=data)
+        self.assertEqual(response.status_code, 201)
+        self.assertTrue(User.objects.exists())
+
+        user = User.objects.get(email='maxime.forestier@free.fr')
+        self.assertEqual(user.first_name, data['first_name'])
+        self.assertEqual(user.last_name, data['last_name'])
+        self.assertEqual(user.email, data['email'])
